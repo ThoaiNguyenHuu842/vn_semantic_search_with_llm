7d23bc86f9a6bf12deeb3eb480695186778cc920
LAPTOP-0OHSO3Q8\thoai <thoainguyen842@gmail.com>
2024-03-31 01:01:07 +0700

    Updat test case

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from sentence_transformers import SentenceTransformer, util
 import torch
 model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
 
-corpus = ['Rolling is a piano store',
-          'Financial instrument is used in finance to describe contracts',
+corpus = ['Bài hát Tết Đến Rồi',
+          'Năm nay nông dân được mùa vụ xuân hè',
           ]
 corpus_embeddings = model.encode(corpus, convert_to_tensor=True)
 
-query = 'Where to buy musical instrument?'
+query = 'Ca khúc về mùa xuân'
 
 query_embedding = model.encode(query, convert_to_tensor=True)
 cos_scores = util.cos_sim(query_embedding, corpus_embeddings)[0]
